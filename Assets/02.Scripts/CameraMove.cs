85fb92bb0ff474b6d7e1b841a86638060f8d5a8a
bnm000215 <53283888+bnm000215@users.noreply.github.com>
2020-04-06 23:02:17 +0900

    SSAO를 추가했음
    추가는 했지만, 아직 적용은 안했음.
    
    URP에서는 아직 정식 적인 SSAO가 없다보니까.. 깃에 올라온 것을 써봤는데,
    퀄리티 있는 SSAO가 아니여서 아쉬움..

---
+++ Assets/02.Scripts/CameraMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    private bool MyView;
+
+    private Vector3 left = new Vector3(23, 1, -41);
+    private Vector3 right = new Vector3(-5, 1, -41);
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            MyView = !MyView;
+        
+        if (MyView)
+            transform.position = Vector3.Lerp(transform.position, left, 3.5f*  Time.deltaTime);
+        else 
+            transform.position = Vector3.Lerp(transform.position, right, 3.5f*  Time.deltaTime);
+    }
+}
