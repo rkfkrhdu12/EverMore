fe98a2f03fe1fad87777e0c28451d5d3d866027c
bnm000215 <53283888+bnm000215@users.noreply.github.com>
2020-04-07 14:02:08 +0900

    유닛 로테이트를 수정했습니다.
    버튼 Pro 수정중
    버튼 그룹 제작 중
    
    인게임 씬 포스트 프로세싱을 건드렸습니다.
    
    인게임 카메라 오브젝트에 카메라 무브 테스트 컴포넌트가 있습니다.

---
+++ Assets/02.Scripts/UI/ButtonGroup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class ButtonGroup : MonoBehaviour
+{
+    [SerializeField,Tooltip("그룹으로 지어줄 버튼들")]
+    private ButtonPro[] buttonPros;
+
+    [SerializeField, Tooltip("초기 선택될 버튼")]
+    private int SelectedNumber = -1;
+
+    private void Awake()
+    {
+        //버튼이 
+        if(buttonPros.Length <1)
+            return;
+
+        foreach (var btnPro in buttonPros)
+            btnPro.isSelected = false;
+
+        if(SelectedNumber == -1)
+            return;
+
+        SelectedNumber = Mathf.Clamp(SelectedNumber, 0, buttonPros.Length);
+        
+        buttonPros[SelectedNumber].isSelected = true;
+    }
+}
