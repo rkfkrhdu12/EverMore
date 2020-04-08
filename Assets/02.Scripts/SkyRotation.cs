471d43686b8d981fbd9ef405899f2c536d8fbf85
bnm000215 <53283888+bnm000215@users.noreply.github.com>
2020-04-08 20:48:50 +0900

    인 게임 오브젝트 사이즈 조정
    인 게임 오브젝트 사이즈를 좀 조정했습니다.
    
    깃발도 펄럭펄럭 ~~

---
+++ Assets/02.Scripts/SkyRotation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class SkyRotation : MonoBehaviour
+{
+    [SerializeField, Tooltip("회전 속도")]
+    private float Speed = 1.3f;
+
+    private static readonly int Rotation = Shader.PropertyToID("_Rotation");
+
+    private void Update()
+    {
+        RenderSettings.skybox.SetFloat(Rotation, Time.time * Speed);
+    }
+}
