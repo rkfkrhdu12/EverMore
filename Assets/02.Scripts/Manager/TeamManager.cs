aff1803ba7a0acbfbb572249c87d207920a73d3d
kimB <rkfkrhdu12@naver.com>
2020-04-14 16:41:18 +0900

    UI들 수정

--- Assets/02.Scripts/Manager/TeamManager.cs
+++ Assets/02.Scripts/Manager/TeamManager.cs
@@ -6,17 +6,33 @@ using TMPro;
 
 public class TeamManager : MonoBehaviour
 {
-    private string _addTeamNameString = "";
+    private string _addTeamNameString = "예비1팀";
 
     public void SetAddTeamNameString(string name) { _addTeamNameString = name; }
-    public string GetAddTeamNameString()          { _textObject.GetComponent<TextMeshPro>().text = ""; return _addTeamNameString; }
+    public string GetAddTeamNameString()
+    {
+        string returnVal = _addTeamNameString;
+
+        _addTeamNameString = "";
+        _textObject.GetComponent<TextMeshPro>().text = "";
+
+        return returnVal;
+    }
+
+    public Team _curSelectTeam = null;
 
     public GameObject _textObject;
 
     public OperateButtonGroup _buttonGroup;
 
+    private void Awake()
+    {
+        AddTeam(GetAddTeamNameString());
+    }
+
     // 팀 이름으로 팀을 찾는다
     private Dictionary<string, Team> _teams = new Dictionary<string, Team>();
+    private List<string> _teamNameList = new List<string>();
 
     public Team GetTeam(string teamName)
     {
@@ -25,5 +41,38 @@ public class TeamManager : MonoBehaviour
         return _teams[teamName];
     }
 
+    public void SelectTeam(GameObject gObject)
+    {
 
+    }
+
+    private void UpdateUI()
+    {
+
+    }
+
+    public void DeleteTeam()
+    {
+
+    }
+
+    public void AddTeam()
+    {
+        string teamName = GetAddTeamNameString();
+        AddTeam(teamName);
+    }
+
+    public void AddTeam(string teamName)
+    {
+        if(string.IsNullOrEmpty(teamName)) { return; }
+
+        Team t = new Team();
+        t.Name = teamName;
+
+        _teams.Add(teamName, t);
+        _teamNameList.Add(teamName);
+
+        // Test
+        _teams[teamName].InitTest();
+    }
 }
