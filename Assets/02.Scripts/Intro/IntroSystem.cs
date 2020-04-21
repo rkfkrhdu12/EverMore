958122788ba30aeca7f90de41d3c8ec4f76a6c5c
bnm000215 <53283888+bnm000215@users.noreply.github.com>
2020-04-22 00:35:06 +0900

    메인 씬 info 창 추가

--- Assets/02.Scripts/Intro/IntroSystem.cs
+++ Assets/02.Scripts/Intro/IntroSystem.cs
@@ -1,21 +1,12 @@
-﻿using GameplayIngredients;
+﻿using System;
+using GameplayIngredients;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class IntroSystem : MonoBehaviour
 {
-    private UInputSystem uis;
-
-    private void Awake()
+    private void Update()
     {
-        uis = new UInputSystem();
-        uis.System.any.performed += AnyOnperformed;
+        if(Input.anyKeyDown)
+            Manager.Get<SceneManagerPro>().LoadScene(SceneManagerPro.eScene.LOBBY);
     }
-    
-    private void AnyOnperformed(InputAction.CallbackContext obj) => 
-        Manager.Get<SceneManagerPro>().LoadScene(SceneManagerPro.eScene.LOBBY);
-
-    private void OnEnable() => uis.Enable();
-
-    private void OnDisable() => uis.Disable();
 }
---
+++ Assets/02.Scripts/MainScene/UI/info.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class info : MonoBehaviour
+{
+    private RectTransform _rect;
+
+    private void Awake() => 
+        _rect = GetComponent<RectTransform>();
+
+    private void Update() => 
+        _rect.position = Input.mousePosition;
+}
