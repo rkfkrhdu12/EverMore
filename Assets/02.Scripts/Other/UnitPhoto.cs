2bfa0ee82d814add6efb078560770533de646910
bnm000215 <53283888+bnm000215@users.noreply.github.com>
2020-04-30 16:55:25 +0900

    모델 교체했숩니다.
    아직 스튜디오 씬은 완성 안됬어요
    
    게임 플레이 인그리디엔트 업데이트 했습니다.
    유니티 2019.3.12f로 올렸는데, 그 이하 버전에서 열어도 문제는 없습니다. ^^;;

--- Assets/02.Scripts/Other/UnitPhoto.cs
+++ Assets/02.Scripts/Other/UnitPhoto.cs
@@ -4,100 +4,36 @@ using UnityEngine;
 public class UnitPhoto : MonoBehaviour
 {
     [System.Serializable]
-    private class PartNameToObj
+    public class PartNameToObj
     {
-        public string name;
-
-        public GameObject head;
-
-        public GameObject body;
+        public string HeadName,BodyName,LeftWeaponName,RightWeaponName;
     }
 
-    [SerializeField]
+    public PartNameToObj partNameToObj;
+    
+    [Space,SerializeField]
     private RenderTexture renderTexture;
-
-    [SerializeField]
-    private PartNameToObj Naked;
-
-    [SerializeField]
-    private PartNameToObj[] partNameToObj;
-
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            //테스트용 유니티 폴더 경로
             StartCoroutine(Iphoto($"{Application.dataPath}/04.Image/Photo"));
+            
+            //실제 릴리즈 폴더 경로
+            //StartCoroutine(Iphoto($"{Application.persistentDataPath}"));
         }
     }
 
     private readonly WaitForSeconds SideTime = new WaitForSeconds(0.1f);
-    
+
     private IEnumerator Iphoto(string path)
     {
-        //Naked.head.SetActive(true);
-        
-        ////중간 텀
-        //yield return SideTime;
-        
-        ////김치
-        //Util.SaveRenderTextuerToPng($"{path}/{Naked.name}-head,{Naked.name}-body.png", renderTexture);
-        
-        ////중간 텀
-        //yield return SideTime;
-        
-        //Naked.head.SetActive(false);
-        
-        ////중간 텀
-        //yield return SideTime;
-        
-        //전체 유닛의 사진을 찍어야하기 때문에 유닛 개수만큼 반복합니다.
-        for (int i = 0; i < partNameToObj.Length; i++)
-        {
-            //풀샷을 찍습니다.
-            partNameToObj[i].head.SetActive(true);
-            partNameToObj[i].body.SetActive(true);
+        Util.SaveRenderTextuerToPng(
+            $"{path}/{partNameToObj.HeadName}-head,{partNameToObj.BodyName}-body,{partNameToObj.LeftWeaponName}-leftWeapon,{partNameToObj.RightWeaponName}-rightWeapon.png", renderTexture);
 
-            //중간 텀
-            yield return SideTime;
-            
-            //김치
-            Util.SaveRenderTextuerToPng(
-                $"{path}/{partNameToObj[i].name}-head,{partNameToObj[i].name}-body.png", renderTexture);
-            
-            //해당 유닛의 바디를 가립니다.
-            partNameToObj[i].body.SetActive(false);
-
-            //남은 파츠를 찍습니다.
-            for (int j = 0; j < partNameToObj.Length; j++)
-            {
-                if (j == i) continue;
-                
-                //다른 파츠 바디를 보이게함.
-                partNameToObj[j].body.SetActive(true);
-                
-                //중간 텀
-                yield return SideTime;
-                
-                //치즈~
-                Util.SaveRenderTextuerToPng(
-                    $"{path}/{partNameToObj[i].name}-head,{partNameToObj[j].name}-body.png", renderTexture);
-                
-                //중간 텀
-                yield return SideTime;
-                
-                //다른 바디가 보일 수 있도록 해당 바디를 가립니다.
-                partNameToObj[j].body.SetActive(false);
-            }
-
-            //해당 유닛 파츠 머리르 가립니다.
-            partNameToObj[i].head.SetActive(false);
-        }
-
-        //전체를 다시 비활성화 합니다.
-        foreach (var t in partNameToObj)
-        {
-            t.head.SetActive(false);
-            t.body.SetActive(false);
-        }
+        //중간 텀
+        yield return SideTime;
     }
 }
